88797f9742a38b99cf4b35bda11b6022b721436a
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 01:00:15 +0530

    Add files via upload

---
+++ multi&divuser.cpp
@@ -0,0 +1,13 @@
+//WAP to multi & div of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    c=a*b;
+    d=a/b;
+    cout<<"add"<<c;
+    cout<<"sub"<<d;
+}
