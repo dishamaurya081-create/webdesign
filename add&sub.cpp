cbaf4e9f778337e5d24b99f1f736527d2cb39693
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 00:51:24 +0530

    Add files via upload

---
+++ add&sub.cpp
@@ -0,0 +1,11 @@
+//WAP to find out add & sub of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=4,b=6,c,d;
+    c=a+b;
+    cout<<"the add is"<<c<<"\n";
+    d=a-b;
+    cout<<"the subtract is"<<d<<"\n";
+}
