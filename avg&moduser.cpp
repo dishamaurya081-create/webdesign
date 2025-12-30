64c233d1406cceb249a2cc83976786b11f9f2eb8
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 00:59:21 +0530

    Add files via upload

---
+++ avg&moduser.cpp
@@ -0,0 +1,11 @@
+//WAP o find out average and modulus of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    c=((a+b)/2);
+    d=a%b;
+    cout<<"avg"<<c;
+    cout<<"mod"<<d;
+}
