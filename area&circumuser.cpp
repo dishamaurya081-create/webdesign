8671b0d9721c118dd82d8b4232df5c052f8823cf
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 00:59:00 +0530

    Add files via upload

---
+++ area&circumuser.cpp
@@ -0,0 +1,12 @@
+//WAP to find area and circumference
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r,area,circum;
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"area is"<<area<<"\n";
+    circum=2*3.14*r;
+    cout<<"circumference"<<circum;
+}
