3a8817b30586d97233bba062b2ca4124af1e290c
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 00:58:38 +0530

    Add files via upload

---
+++ adduser.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+//WAP to find add of 3 numbers entered by user
+int main()
+{
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    d=a+b+c;
+    cout<<"add is"<<d;
+}
+//WAP to find add&sub of two numbers
+int main()
+{
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    c=a+b;
+    d=a-b;
+    cout<<"add"<<c<<"\n";
+    cout<<"sub"<<d;
+}
