4e65fd34edd660590d10ef505ceb6e5662c81b3d
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 01:01:15 +0530

    Add files via upload

---
+++ useoflinechange.cpp
@@ -0,0 +1,13 @@
+// WAP to write name,father's name,branch,section,address,gender
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"NAME \t-\t Disha maurya\n";
+    cout<<"FATHER'S NAME \t-\t Govind prasad maurya\n";
+    cout<<"BRANCH \t-\t CSE\n";
+    cout<<"SECTION \t-\t 17\n";
+    cout<<"GENDER \t-\t FEMALE\n";
+}
