a49c1b38cef14484d072571e594a072b801a3a2e
dishamaurya081-create <dishamaurya081@gmail.com>
2025-12-31 00:51:44 +0530

    Add files via upload

---
+++ add,sub,multi,divuser.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f;
+    cin>>a;
+    cin>>b;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    cout<<"add"<<c;
+    cout<<"sub"<<d;
+    cout<<"multi"<<e;
+    cout<<"div"<<f;
+}
